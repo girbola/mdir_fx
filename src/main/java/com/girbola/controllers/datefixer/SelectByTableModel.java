983dfa50c71e3318b235d982c7ca8f2825ee231a
Marko Lokka <e1901043@edu.vamk.fi>
2020-08-11 14:57:16 +0300

    SelectByTableModel created
    This will work in every EXIF_Data_Selector model. Now there are 4. Event, location, dates and camera model

---
+++ src/main/java/com/girbola/controllers/datefixer/SelectByTableModel.java
@@ -0,0 +1,83 @@
+package com.girbola.controllers.datefixer;
+
+import java.util.Iterator;
+
+import com.girbola.Main;
+import com.girbola.fileinfo.FileInfo;
+import com.girbola.messages.Messages;
+
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.scene.Node;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+
+public class SelectByTableModel extends Task<Boolean> {
+
+//	private ObservableList<Node> children;
+	private String type;
+	private TableView<EXIF_Data_Selector> tableView;
+	private Model_datefix model_datefix;
+
+	public SelectByTableModel(Model_datefix model_datefix, String type, TableView<EXIF_Data_Selector> aTableView) {
+		super();
+		this.model_datefix = model_datefix;
+		this.type = type;
+		this.tableView = aTableView;
+	}
+
+	@Override
+	protected Boolean call() throws Exception {
+		Iterator<Node> it = model_datefix.getGridPane().getChildren().iterator();
+		Messages.sprintf("===============" + type + " tableView");
+		while (it.hasNext()) {
+			Node node = it.next();
+			if (node instanceof VBox && node.getId().equals("imageFrame")) {
+				FileInfo fileInfo = (FileInfo) node.getUserData();
+
+				if (hasNewValue(fileInfo)) {
+					Platform.runLater(() -> {
+						model_datefix.getSelectionModel().addOnly(node);
+					});
+				} else {
+					Platform.runLater(() -> {
+						model_datefix.getSelectionModel().remove(node);
+					});
+				}
+			}
+		}
+		return null;
+	}
+
+	private boolean hasNewValue(FileInfo fileInfo) {
+		Messages.sprintf("HASVALUE: type is: " + type);
+		Iterator<EXIF_Data_Selector> it = tableView.getSelectionModel().getSelectedItems().iterator();
+		SelectorModelType sbt = SelectorModelType.valueOf(type);
+		String dataModel = "";
+		switch (sbt) {
+		case CAMERA:
+			dataModel = fileInfo.getCamera_model();
+			break;
+		case EVENT:
+			dataModel = fileInfo.getEvent();
+			break;
+		case DATE:
+			dataModel = Main.simpleDates.getSdf_ymd_minus().format(fileInfo.getDate());
+			Messages.sprintf("dataModel is " + dataModel);
+			break;
+		case LOCATION:
+			dataModel = fileInfo.getLocation();
+			break;
+		}
+
+		while (it.hasNext()) {
+			EXIF_Data_Selector eds = it.next();
+			Messages.sprintf("Edt is: " + eds.getInfo() + " type is:  " + type + " datamodel " + dataModel);
+			if (eds.getInfo().equals(dataModel)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
