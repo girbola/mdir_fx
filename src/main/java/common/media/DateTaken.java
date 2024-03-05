2dafb46e1c2a0718b3ca472c4c5ec69ab9a62604
girbola <girbola@users.noreply.github.com>
2024-03-05 20:31:47 +0200

    Refactor DateTaken methods for clarity and efficiency
    Streamlined variable assignment and return statements in the DateTaken class. Removed unnecessary variable assignments and simplified code structures for improved readability and efficiency. This included cleaning up the structure of if-else statements and improving the arrangement of variable declarations.

--- src/main/java/common/media/DateTaken.java
+++ src/main/java/common/media/DateTaken.java
@@ -28,127 +28,114 @@ import java.util.Date;
 import static com.girbola.messages.Messages.sprintf;
 
 /**
- *
  * @author Marko Lokka
  */
 public class DateTaken {
 
-	public static String getCameraModel(Metadata metaData) {
-		Directory directory2 = metaData.getFirstDirectoryOfType(ExifIFD0Directory.class);
-		String model = null;
-		try {
-			model = directory2.getString(0x110);
-		} catch (Exception e) {
-			return null;
-		}
-		return model;
-	}
+    public static String getCameraModel(Metadata metaData) {
 
-	public static int getMetaDataOrientation(Metadata metaData) {
-		int orientation = 0;
-		Directory directory = metaData.getFirstDirectoryOfType(ExifIFD0Directory.class);
-		if (directory != null) {
-			try {
-				orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
-			} catch (MetadataException ex) {
-				return 0;
-			}
-		}
-		return orientation;
-	}
+        Directory directory2 = metaData.getFirstDirectoryOfType(ExifIFD0Directory.class);
+        try {
+            return directory2.getString(0x110);
+        } catch (Exception e) {
+            return null;
+        }
 
-	public static long getCreationDate(Path path) {
-		if (FileUtils.supportedImage(path)) {
-			long date = getMetadataDateTaken(path);
-			if (date != 0) {
-				return date;
-			}
-		} else if (FileUtils.supportedVideo(path)) {
-			String orgFileName = FileNameParseUtils.parseFileExtentension(path);
-			Path thmFile = Paths.get(path.getParent() + File.separator + orgFileName + ".THM");
-			sprintf("thmFile name is: " + thmFile);
-			if (Files.exists(thmFile)) {
-				sprintf("THM FILE FOUND: " + thmFile);
-				// ongelma;
-				long date = getMetadataDateTaken(thmFile);
-				sprintf("getDateThumbFileForVideo date: " + date);
-				return date;
-			} else {
-				sprintf("THM FILE NOT FOUND: " + thmFile);
-				return 0;
-			}
-		}
-		return 0;
-	}
+    }
 
-	public static long getMetadataDateTaken(Path path) {
-		Metadata metaData = null;
-		try {
-			metaData = ImageMetadataReader.readMetadata(path.toFile());
-		} catch (ImageProcessingException
-				| IOException ex) {
-			return 0;
-		}
-		Date date = null;
-		Iterable<Directory> directories = metaData.getDirectories();
+    public static int getMetaDataOrientation(Metadata metaData) {
 
-		for (Directory directory : directories) {
-			if (directory != null) {
-				date = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-				if (date != null) {
-					return date.getTime();
-				}
-			}
-		}
-		if (date == null) {
-			return 0;
-		}
-		return 0;
-	}
+        Directory directory = metaData.getFirstDirectoryOfType(ExifIFD0Directory.class);
+        if (directory != null) {
+            try {
+                return directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+            } catch (MetadataException ex) {
+                return 0;
+            }
+        }
+        return 0;
+    }
 
-	public static long getMetaDataCreationDate(Metadata metaData, Path path) {
-		// sprintf("getMetadataCreationDate: " + path);
+    public static long getCreationDate(Path path) {
+        if (FileUtils.supportedImage(path)) {
+            long date = getMetadataDateTaken(path);
+            if (date != 0) {
+                return date;
+            }
+        } else if (FileUtils.supportedVideo(path)) {
+            String orgFileName = FileNameParseUtils.parseFileExtentension(path);
+            Path thmFile = Paths.get(path.getParent() + File.separator + orgFileName + ".THM");
+            sprintf("thmFile name is: " + thmFile);
+            if (Files.exists(thmFile)) {
+                sprintf("THM FILE FOUND: " + thmFile);
+                // ongelma;
+                long date = getMetadataDateTaken(thmFile);
+                sprintf("getDateThumbFileForVideo date: " + date);
+                return date;
+            } else {
+                sprintf("THM FILE NOT FOUND: " + thmFile);
+                return 0;
+            }
+        }
+        return 0;
+    }
 
-		Iterable<Directory> directories = metaData.getDirectories();
+    public static long getMetadataDateTaken(Path path) {
+        Metadata metaData = null;
+        try {
+            metaData = ImageMetadataReader.readMetadata(path.toFile());
+        } catch (ImageProcessingException
+                 | IOException ex) {
+            return 0;
+        }
+        Iterable<Directory> directories = metaData.getDirectories();
 
-		if (FileUtils.supportedImage(path.toFile()) || FileUtils.supportedRaw(path.toFile())) {
-			for (Directory directory : directories) {
-				if (directory != null) {
-					Date imageDate_EXIF = null;
-					imageDate_EXIF = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-					if (imageDate_EXIF != null) {
-						return imageDate_EXIF.getTime();
-					}
-				}
-			}
-		} else if (FileUtils.supportedVideo(path)) {
-			for (Directory directory : directories) {
-				if (directory != null) {
-					Date videoDate_EXIF = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
-					if (videoDate_EXIF != null) {
-						return videoDate_EXIF.getTime();
-					}
-					Date tag_creation_time_QUICKTIME = directory.getDate(QuickTimeDirectory.TAG_CREATION_TIME);
-					if (tag_creation_time_QUICKTIME != null) {
-						return tag_creation_time_QUICKTIME.getTime();
-					}
-					Date tag_creation_time_MP4 = directory.getDate(Mp4VideoDirectory.TAG_CREATION_TIME);
-					if (tag_creation_time_MP4 != null) {
-						return tag_creation_time_MP4.getTime();
-					}
-				}
-			}
-		}
-		return 0;
-	}
+        for (Directory directory : directories) {
+            if (directory != null) {
+                return directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL).getTime();
+            }
+        }
+        return 0;
+    }
 
-	public static Metadata readMetaData(Path path) {
-		Metadata metaData = null;
-		try {
-			metaData = ImageMetadataReader.readMetadata(path.toFile());
-		} catch (Exception e) {
-			return null;
-		}
-		return metaData;
-	}
+    public static long getMetaDataCreationDate(Metadata metaData, Path path) {
+
+        Iterable<Directory> directories = metaData.getDirectories();
+
+        if (FileUtils.supportedImage(path.toFile()) || FileUtils.supportedRaw(path.toFile())) {
+            for (Directory directory : directories) {
+                if (directory != null) {
+                    return directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL).getTime();
+                }
+            }
+        } else if (FileUtils.supportedVideo(path)) {
+            for (Directory directory : directories) {
+                if (directory != null) {
+                    Date videoDate_EXIF = directory.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
+                    if (videoDate_EXIF != null) {
+                        return videoDate_EXIF.getTime();
+                    }
+                    Date tag_creation_time_QUICKTIME = directory.getDate(QuickTimeDirectory.TAG_CREATION_TIME);
+                    if (tag_creation_time_QUICKTIME != null) {
+                        return tag_creation_time_QUICKTIME.getTime();
+                    }
+                    Date tag_creation_time_MP4 = directory.getDate(Mp4VideoDirectory.TAG_CREATION_TIME);
+                    if (tag_creation_time_MP4 != null) {
+                        return tag_creation_time_MP4.getTime();
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static Metadata readMetaData(Path path) {
+
+        try {
+            return ImageMetadataReader.readMetadata(path.toFile());
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
 }
