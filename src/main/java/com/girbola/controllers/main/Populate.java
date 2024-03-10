052de9a8a469664fead1ae97fe6ecbaa620f2b60
girbola <girbola@users.noreply.github.com>
2024-03-10 20:03:07 +0200

    Refactor Populate.java for improved file handling performance
    Efficiency has been improved in the populateTables_FolderScanner_list method by using a lambda expression instead of an anonymous handler event. Variable names have been also made more expressive for better understanding. The refactor aims to enhance the speed of the file handling process.

--- src/main/java/com/girbola/controllers/main/Populate.java
+++ src/main/java/com/girbola/controllers/main/Populate.java
@@ -33,166 +33,141 @@ import static com.girbola.concurrency.ConcurrencyUtils.getExecCounter;
 import static com.girbola.messages.Messages.sprintf;
 
 /**
- *
  * @author Marko Lokka
  */
 public class Populate {
 
-	private static final String ERROR = Populate.class.getSimpleName();
+    private static final String ERROR = Populate.class.getSimpleName();
 
-	private Model_main model_main;
+    private Model_main model_main;
 
-	private IntegerProperty total = new SimpleIntegerProperty();
+    private IntegerProperty total = new SimpleIntegerProperty();
 
-	private AtomicInteger process = new AtomicInteger(0);
+    private AtomicInteger process = new AtomicInteger(0);
 
-	public Populate(Model_main model) {
-		this.model_main = model;
-		ConcurrencyUtils.initSingleExecutionService();
-	}
+    public Populate(Model_main model) {
+        this.model_main = model;
+        ConcurrencyUtils.initSingleExecutionService();
+    }
 
-	public void populateTables_FolderScanner_list(Window owner) {
-		sprintf("SorterTest action started");
-		Main.setProcessCancelled(false);
+    public void populateTables_FolderScanner_list(Window owner) {
+        sprintf("SorterTest action started");
+        Main.setProcessCancelled(false);
 
-		if (model_main.getSelectedFolders().getSelectedFolderScanner_obs().isEmpty()) {
-			sprintf("getSelection_FolderScanner list were empty");
-			return;
-		}
-		model_main.tables().getSortIt_table().getItems().clear();
-		model_main.tables().getSorted_table().getItems().clear();
+        if (model_main.getSelectedFolders().getSelectedFolderScanner_obs().isEmpty()) {
+            sprintf("getSelection_FolderScanner list were empty");
+            return;
+        }
+        model_main.tables().getSortIt_table().getItems().clear();
+        model_main.tables().getSorted_table().getItems().clear();
 
-		/*
-		 * Load from selectedFolder list Sort to tables Calculate tables content
-		 */
-		List<Path> list = new ArrayList<>();
-		for (SelectedFolder sf : model_main.getSelectedFolders().getSelectedFolderScanner_obs()) {
-			if (!hasInIgnoredListMain(Main.conf.getIgnoredFoldersScanList(), sf.getFolder())) {
-				if (sf.isConnected()) {
-					list.add(Paths.get(sf.getFolder()));
-					sprintf("Path is: " + sf + " isConnected: " + sf.isConnected());
-				}
-			}
-		}
-		if (list.isEmpty()) {
-			Messages.warningText("No selected folder(s) to scan. Choose \"File/Add folders\" to choose folder to scan");
-			return;
-		}
-		for (Path pt : list) {
-			sprintf("SelectedFolder is: " + pt);
-		}
-		Task<List<Path>> createFileList = new SubList(list);
+        /*
+         * Load from selectedFolder list Sort to tables Calculate tables content
+         */
+        List<Path> selectedFolders = new ArrayList<>();
+        for (SelectedFolder sf : model_main.getSelectedFolders().getSelectedFolderScanner_obs()) {
+            if (!hasInIgnoredListMain(Main.conf.getIgnoredFoldersScanList(), sf.getFolder())) {
+                if (sf.isConnected()) {
+                    selectedFolders.add(Paths.get(sf.getFolder()));
+                    sprintf("Path is: " + sf + " isConnected: " + sf.isConnected());
+                }
+            }
+        }
+        if (selectedFolders.isEmpty()) {
+            Messages.warningText("No selected folder(s) to scan. Choose \"File/Add folders\" to choose folder to scan");
+            return;
+        }
+        for (Path pt : selectedFolders) {
+            sprintf("SelectedFolder is: " + pt);
+        }
+        Task<List<Path>> createFileList = new SubList(selectedFolders);
 
-		LoadingProcessTask loadingProcess_task = new LoadingProcessTask(owner);
-		createFileList.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-			@Override
-			public void handle(WorkerStateEvent event) {
-				List<Path> list = null;
-				try {
-					list = createFileList.get();
-				} catch (Exception ex) {
-					Messages.sprintfError("Something went wrong with createing filelist");
-					Messages.errorSmth(ERROR, "", ex, Misc.getLineNumber(), true);
-				}
-				if (list.isEmpty()) {
-					Messages.warningText("List is empty at Populate class. Cancelling");
-					//Main.setProcessCancelled(true);
-					return;
-				}
-				Collections.sort(list);
+        LoadingProcessTask loadingProcess_task = new LoadingProcessTask(owner);
+        createFileList.setOnSucceeded(succeeded -> {
+            List<Path> fileList = null;
+            try {
+                fileList = createFileList.get();
+            } catch (Exception ex) {
+                Messages.sprintfError("Something went wrong with createing filelist");
+                Messages.errorSmth(ERROR, "", ex, Misc.getLineNumber(), true);
+            }
+            if (fileList.isEmpty()) {
+                Messages.warningText("List is empty at Populate class. Cancelling");
+                //Main.setProcessCancelled(true);
+                return;
+            }
+            Collections.sort(fileList);
 
-				Task<Integer> sorter = new Sorter(model_main, list);
-				loadingProcess_task.setTask(sorter);
-				sorter.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-					@Override
-					public void handle(WorkerStateEvent event) {
-						total.set(model_main.tables().getAsItIs_table().getItems().size()
-								+ model_main.tables().getSortIt_table().getItems().size()
-								+ model_main.tables().getSorted_table().getItems().size());
-						process.set(total.get());
-						sprintf("sorter.setOnSucceeded total: " + total);
-						loadingProcess_task.setTask(sorter);
-						loadingProcess_task.setMessage("Sorter");
-						//Folder content checkki tähän. TArkistaa että onko tiedostot olemassa. Ja palauttaa varoituksen jos ei löydy tiedostoja.
-						
-						Task<Void> calculateFolderContent = new CalculateFolderContent(model_main, loadingProcess_task, total);
-						loadingProcess_task.setTask(calculateFolderContent);
-						calculateFolderContent.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-							@Override
-							public void handle(WorkerStateEvent event) {
-								try {
-									process.set(process.get() - 1);
-									loadingProcess_task.setMessage("Saving...");
-									// XMLFunctions.saveAll(model.getTables());
-									loadingProcess_task.closeStage();
-									model_main.getMonitorExternalDriveConnectivity().restart();
-									
-									sprintf("calculateFolderContent setOnSucceeded: " + sorter.get());
-								} catch (InterruptedException | ExecutionException ex) {
-									Messages.errorSmth(ERROR, "", ex, Misc.getLineNumber(), true);
-								}
-							}
-						});
-						calculateFolderContent.setOnCancelled(new EventHandler<WorkerStateEvent>() {
-							@Override
-							public void handle(WorkerStateEvent event) {
-								sprintf("calculateFolderContent setOnCancelled");
-							}
-						});
-						calculateFolderContent.setOnFailed(new EventHandler<WorkerStateEvent>() {
-							@Override
-							public void handle(WorkerStateEvent event) {
-								sprintf("calculateFolderContent setOnFailed");
-								loadingProcess_task.setMessage("FAILED...");
-								Messages.errorSmth(ERROR, "", null, Misc.getLineNumber(), true);
-								loadingProcess_task.closeStage();
-							}
-						});
-						exec[getExecCounter()].submit(calculateFolderContent);
-					}
-				});
-				sorter.setOnCancelled(new EventHandler<WorkerStateEvent>() {
-					@Override
-					public void handle(WorkerStateEvent event) {
-						sprintf("sorter.setOnCancelled");
-						loadingProcess_task.closeStage();
+            Task<Integer> sorter = new Sorter(model_main, fileList);
+            loadingProcess_task.setTask(sorter);
+            sorter.setOnSucceeded(succeede -> {
+                    total.set(model_main.tables().getAsItIs_table().getItems().size()
+                            + model_main.tables().getSortIt_table().getItems().size()
+                            + model_main.tables().getSorted_table().getItems().size());
+                    process.set(total.get());
+                    sprintf("sorter.setOnSucceeded total: " + total);
+                    loadingProcess_task.setTask(sorter);
+                    loadingProcess_task.setMessage("Sorter");
+                    //Folder content checkki tähän. TArkistaa että onko tiedostot olemassa. Ja palauttaa varoituksen jos ei löydy tiedostoja.
 
-					}
-				});
-				sorter.setOnFailed(new EventHandler<WorkerStateEvent>() {
-					@Override
-					public void handle(WorkerStateEvent event) {
-						loadingProcess_task.setMessage("FAILED...");
+                    Task<Void> calculateFolderContent = new CalculateFolderContent(model_main, loadingProcess_task, total);
+                    loadingProcess_task.setTask(calculateFolderContent);
+                    calculateFolderContent.setOnSucceeded(success -> {
+                        try {
+                            process.set(process.get() - 1);
+                            loadingProcess_task.setMessage("Saving...");
+                            // XMLFunctions.saveAll(model.getTables());
+                            loadingProcess_task.closeStage();
+                            model_main.getMonitorExternalDriveConnectivity().restart();
 
-						sprintf("sorter.setOnFailed");
-						loadingProcess_task.closeStage();
+                            sprintf("calculateFolderContent setOnSucceeded: " + sorter.get());
+                        } catch (InterruptedException | ExecutionException ex) {
+                            Messages.errorSmth(ERROR, "", ex, Misc.getLineNumber(), true);
+                        }
+                    });
+                    calculateFolderContent.setOnCancelled(cancelled -> {
+                        sprintf("calculateFolderContent setOnCancelled");
+                    });
+                    calculateFolderContent.setOnFailed(failed -> {
+                        sprintf("calculateFolderContent setOnFailed");
+                        loadingProcess_task.setMessage("FAILED...");
+                        Messages.errorSmth(ERROR, "", null, Misc.getLineNumber(), true);
+                        loadingProcess_task.closeStage();
+                    });
+                    exec[getExecCounter()].submit(calculateFolderContent);
+            });
+            sorter.setOnCancelled(cancelled -> {
+                sprintf("sorter.setOnCancelled");
+                loadingProcess_task.closeStage();
+            });
+            sorter.setOnFailed(failed -> {
+                loadingProcess_task.setMessage("FAILED...");
+                sprintf("sorter.setOnFailed");
+                loadingProcess_task.closeStage();
+            });
 
-					}
-				});
-
-				exec[getExecCounter()].execute(sorter);
+            exec[getExecCounter()].execute(sorter);
 //				Thread sorter_th = new Thread(sorter, "sorter_th");
 //				sprintf("sorter_th: " + sorter_th.getName());
 //				sorter_th.start();
-			}
-		});
-		createFileList.setOnCancelled(e->{
+        });
+        createFileList.setOnCancelled(e -> {
 
-		});
-		createFileList.setOnFailed(e->{
+        });
+        createFileList.setOnFailed(e -> {
 
-		});
-		Thread createFileList_th = new Thread(createFileList, "createFileList_th");
-		sprintf("createFileList_th.getName(): " + createFileList_th.getName());
-		createFileList_th.start();
-	}
+        });
+        Thread createFileList_th = new Thread(createFileList, "createFileList_th");
+        sprintf("createFileList_th.getName(): " + createFileList_th.getName());
+        createFileList_th.start();
+    }
 
-	private boolean hasInIgnoredListMain(ObservableList<Path> ignoredList, String path) {
-		for (Path p : ignoredList) {
-			if (p.toString().equals(path)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    private boolean hasInIgnoredListMain(ObservableList<Path> ignoredList, String path) {
+        for (Path p : ignoredList) {
+            if (p.toString().equals(path)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
